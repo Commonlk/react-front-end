688f8cc6f4006fa5aedc900867fcaae753f35173
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-06-29 11:59:49 -0300

    Add import for the UserList, Create dummy USER arr

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+      name: "Martha Ferdinand",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
