08a16dc7fe5b8f45aba64791cf17bb20b80c1969
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-07-08 19:52:51 -0300

    Fix typos

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -12,7 +12,7 @@ const formReducer = (state, action) => {
   switch (action.type) {
     case "INPUT_CHANGE":
       let formIsValid = true;
-      for (const inputId in state.input) {
+      for (const inputId in state.inputs) {
         if (inputId === action.inputId) {
           formIsValid = formIsValid && action.isValid;
         } else {
@@ -21,7 +21,7 @@ const formReducer = (state, action) => {
       }
       return {
         ...state,
-        input: {
+        inputs: {
           ...state.inputs,
           [action.inputId]: { value: action.value, isValid: action.isValid },
         },
