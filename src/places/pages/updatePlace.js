9dbe67ebb40ff65d070e735d494ce1b00ae2a36b
Victor <vitinsilva0xd@gmail.com>
2021-10-26 15:59:57 -0300

    Change hardcode api urls to env based ones

--- src/places/pages/updatePlace.js
+++ src/places/pages/updatePlace.js
@@ -40,7 +40,7 @@ const UpdatePlace = () => {
     const getPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
 
         setloadedPlace(responseData.place);
@@ -67,7 +67,7 @@ const UpdatePlace = () => {
 
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         "PATCH",
         JSON.stringify({
           title: formState.inputs.title.value,
