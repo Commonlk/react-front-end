3ac03c21274c2d215c329d3ffc692060973c0671
Victor <vitinsilva0xd@gmail.com>
2021-10-26 12:56:33 -0300

    Add to auth login the new userId and token

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -73,7 +73,7 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     } else {
       try {
@@ -87,7 +87,7 @@ const Auth = () => {
           "POST",
           formData
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     }
   };
